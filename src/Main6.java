3b333bdd91829d563f41dc6654d4f424439869ad
ted7088 <ted3131@naver.com>
2023-07-28 17:43:39 +0900

    commit

---
+++ src/Main6.java
@@ -0,0 +1,28 @@
+//부분집합 구하기 (DFS)
+public class Main6 {
+    static int n;
+    static int[] ch;
+
+    public void DFS(int L) {
+        if (L == n + 1) {
+            String tmp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) tmp += (i + " ");
+            }
+            if (tmp.length() > 0) System.out.println(tmp);
+
+        } else {
+            ch[L] = 1; //사용하는 원소
+            DFS(L + 1);//왼쪽으로 뻣는 트리
+            ch[L] = 0; //사용하지 않는 원소
+            DFS(L + 1);//오른쪽으로 뻣는 트리
+        }
+    }
+
+    public static void main(String[] args) {
+        Main6 T = new Main6();
+        n = 3;
+        ch = new int[n + 1];
+        T.DFS(1);
+    }
+}
